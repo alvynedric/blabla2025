649cd2f436452ae0d2694142db7187672f142795
alvynedric <edricalvyn@gmail.com>
2025-07-05 14:11:01 +0700

    algoritma pseudocode

---
+++ algoritmapseudocode.js
@@ -0,0 +1,33 @@
+// ALGORITMA PSEUDOCODE
+ 
+/**
+ * STUDY CASE 1
+ * 
+ *  Tentukan algoritma dan pseudocode untuk menghitung luas persegi panjang
+ * buat variable panjang
+ * buat variable lebar
+ * buat variable luas
+ * isi luas dengan panjang kali lebar
+ * cetak luas
+ * 
+ * Pseudocode
+ * STORE panjang with ANY NUMBER
+ * STORE lebar with ANY NUMBER
+ * 
+ * STORE luas WITH panjang MUL lebar
+ * PRINT luas
+ */
+/**
+ * Study Case 2
+ * Tentukan Algoritma dan pseudocode untuk luas lingkaran
+ * 
+ * Algoritma 
+ * masukkan variable jari-jari
+ * hitung luas dengan rumus 3.14 * jari-jari * jari-jari
+ * cetak luas
+ * 
+ * Pseudocode
+ * STORE jari-jari WITH ANY NUMBER
+ * STORE luas WITH 3.14 MUL jari-jari MUL jari-jari
+ * PRINT luas
+ */
