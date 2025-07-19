55b6cd3e8d66c681eaec078a784e97aadc068762
alvynedric <edridalvyn@gmail.com>
2025-07-19 10:42:45 +0700

    factoryclass

--- inheritance.js
+++ inheritance.js
@@ -22,12 +22,18 @@ class Motorcycle extends Vehicle {
         super(id, name, "Motorcycle", price, isSold);
     }
 }
+class Sport extends Vehicle {
+    constructor(id, name, price, isSold){
+        super(id, name, "Sport", price, isSold);
+    }
+}
 // instanciation
 let civic = new Sedan(1, "Civic Turbo", 1000000, true);
 let hino = new Truck(2, "Hino Dutro", 500000, false);
 let tiger = new Motorcycle(3, "Tiger 2000", 200000, true);
+let yamahar1m = new Sport(4, "Yamaha R1M", 10000000, true);
 
 console.log(civic);
 console.log(hino);
 console.log(tiger);
-
+console.log(yamahar1m);
